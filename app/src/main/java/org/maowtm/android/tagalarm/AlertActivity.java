1a3c6299eed75b2b76e6d130837cd9e4b7f32c49
Wtm <micromaomao@gmail.com>
2016-08-10 22:38:13 +0800

    Implement wait

--- app/src/main/java/org/maowtm/android/tagalarm/AlertActivity.java
+++ app/src/main/java/org/maowtm/android/tagalarm/AlertActivity.java
@@ -21,7 +21,7 @@ public class AlertActivity extends Activity {
             try {
                 AlertActivity.this.updateTime();
             } finally {
-                AlertActivity.this.updateTimeHandler.postDelayed(this, 1000);
+                AlertActivity.this.updateTimeHandler.postDelayed(this, 500);
             }
         }
     }
@@ -34,26 +34,29 @@ public class AlertActivity extends Activity {
     protected String proofOfWake;
     protected ProofOfWakes pows;
     protected String failure;
+    protected TextView powstuffWait;
     @Override
     protected void onCreate(Bundle sis) {
         super.onCreate(sis);
         this.alarmId = this.getIntent().getLongExtra(Alarms.INTENT_EXTRA_ALARM_ID, -1);
         this.allowDirectDismiss = this.getIntent().getBooleanExtra(Alarms.INTENT_EXTRA_ALLOW_DIRECT_DISMISS, false);
         this.proofOfWake = this.getIntent().getStringExtra(Alarms.INTENT_EXTRA_PROOF_OF_WAKES);
+        this.triggerTime = System.currentTimeMillis();
         if (this.proofOfWake == null) {
             this.proofOfWake = "[]";
         }
         if (sis != null) {
-            this.triggerTime = sis.getLong("triggerTime", System.currentTimeMillis());
+            this.triggerTime = sis.getLong("triggerTime", this.triggerTime);
             this.allowDirectDismiss = sis.getBoolean("allowDirectDismiss", this.allowDirectDismiss);
             this.alarmId = sis.getLong("alarmId", this.alarmId);
             this.proofOfWake = sis.getString("proofOfWake", this.proofOfWake);
         }
 
         try {
-            pows = new ProofOfWakes(new JSONArray(this.proofOfWake));
+            this.pows = new ProofOfWakes(new JSONArray(this.proofOfWake));
         } catch (JSONException e) {
             this.failure = getString(R.string.alert_failure_msg) + "\n" + e.getMessage();
+            this.pows = null;
             // TODO: Show this in UI.
         }
         this.requestWindowFeature(android.view.Window.FEATURE_NO_TITLE);
@@ -66,6 +69,8 @@ public class AlertActivity extends Activity {
         this.setContentView(R.layout.alertlayout);
 
         this.timeView = (TextView) this.findViewById(R.id.timeView);
+        this.powstuffWait = (TextView) this.findViewById(R.id.powstuff_wait);
+
         this.updateTimeHandler = new Handler();
         this.updateTimeRunnable = new UpdateTimeRunnable();
         this.findViewById(R.id.dismiss).setOnClickListener(new View.OnClickListener() {
@@ -108,7 +113,21 @@ public class AlertActivity extends Activity {
         this.updateTimeRunnable.run();
     }
     protected void updateTime() {
-        CharSequence time = this.timeFormat.format(new Date());
+        Date now = new Date();
+        CharSequence time = this.timeFormat.format(now);
+
+        if (this.pows != null) {
+            int seconds = this.pows.getWaitTime();
+            if (seconds <= 60*60*24) {
+                int escaped = (int)(now.getTime() - this.triggerTime) / 1000;
+                if (escaped >= seconds) {
+                    this.startDismiss();
+                } else {
+                    this.powstuffWait.setVisibility(View.VISIBLE);
+                    this.powstuffWait.setText(this.getString(R.string.powstuff_wait, seconds - escaped));
+                }
+            }
+        }
         this.timeView.setText(time);
     }
     protected void startDismiss() {
